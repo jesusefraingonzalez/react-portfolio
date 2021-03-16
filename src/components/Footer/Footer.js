415e4ef2d3c6e219e74d51316d9bd0fb8b1519f1
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-16 14:21:55 -0400

    add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer {
+    render() {
+        <footer class="bg-dark text-center text-lg-start">
+            <div class="text-center p-3" style="color:whitesmoke">
+                <a href="https://www.github.com/jesusefraingonzalez"><button>Github</button></a>
+                <a
+                    href="https://www.linkedin.com/in/efra%C3%ADn-gonz%C3%A1lez-aa6233123/"><button>linkedIn</button></a>
+                <a href="./resume.pdf"><button>Resumé</button></a>
+            </div>
+        </footer>
+    }
+}
