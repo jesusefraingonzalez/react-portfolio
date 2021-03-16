fde5bc9885c054075ba75f5aaa40bea07a35a11b
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-16 15:53:37 -0400

    format

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,15 @@ class Footer extends Component {
         return (
             <footer className="bg-dark text-center text-lg-start">
                 <div className="text-center p-3" >
-                    <a href="https://www.github.com/jesusefraingonzalez"><button>Github</button></a>
-                    <a
-                        href="https://www.linkedin.com/in/efra%C3%ADn-gonz%C3%A1lez-aa6233123/"><button>linkedIn</button></a>
-                    <a href="./resume.pdf"><button>Resumé</button></a>
+                    <a href="https://www.github.com/jesusefraingonzalez">
+                        <button>Github</button>
+                    </a>
+                    <a href="https://www.linkedin.com/in/efra%C3%ADn-gonz%C3%A1lez-aa6233123/">
+                        <button>linkedIn</button>
+                    </a>
+                    <a href="./resume.pdf">
+                        <button>Resumé</button>
+                    </a>
                 </div>
             </footer>
         );
