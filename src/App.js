50087603cfb6c3b1891a8be26261974d9adfe14d
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-16 21:36:56 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import './App.css';
-import Container from './components/Container/Container';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Container />
+      <Switch>
+        <Route exact path="/">
+          <About />
+        </Route>
+        <Route exact path="/contact.html">
+          <Contact />
+        </Route>
+        <Route exact path="/portfolio.html">
+          <Portfolio />
+        </Route>
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
